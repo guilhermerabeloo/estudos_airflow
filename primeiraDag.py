681e1ec2c2dd508170ba2fd089cd4301bbcf5720
guilhermerabeloo <guilhermerabelo699@gmail.com>
2024-09-30 20:53:45 -0300

    Feat/First dag

---
+++ primeiraDag.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+dag = DAG('primeira_dag', 
+          description='Hello World',
+          schedule_interval=None,
+          start_date=datetime(2023, 9, 30),
+          catchup=False)
+
+task1 = BashOperator(task_id='tsk1', bash_command="sleep 5", dag=dag)
+task2 = BashOperator(task_id='tsk2', bash_command="sleep 5", dag=dag)
+task3 = BashOperator(task_id='tsk3', bash_command="sleep 5", dag=dag)
+
+
+task1 >> task2 >> task3
+
